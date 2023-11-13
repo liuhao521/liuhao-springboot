b4c6da48ca4d0234d11883a87afb739d65d2b7b3
yh_liuhao <liuhao@yinhai.com>
2023-11-13 09:58:45 +0800

    add: init

---
+++ test/src/test/java/com/liuhao/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuhao.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
